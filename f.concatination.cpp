a47c221f1d1ffc12fd68548ef0d5f51d8e75c212
sajid004 <hafizsajid.645@gmail.com>
2017-07-31 12:29:25 +0500

    string function
    concatenation in c++

---
+++ f.concatination.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<conio.h>
+#include <string>
+using namespace std;
+main ()
+{
+  string firstlevel("salman");
+  string secondlevel("khan");
+  string hostname;
+  hostname=secondlevel+firstlevel;
+  cout <<hostname;
+  getch();
+  return 0;
+}
